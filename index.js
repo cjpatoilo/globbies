12f929f058473dfe6cfcc1d7973fd46e14642fcf
CJ Patoilo <cjpatoilo@gmail.com>
2016-12-16 10:28:05 -0300

    Remove file check

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 'use strict'
-const fs = require('fs')
-const glob = require("glob")
+const glob = require('glob')
 const path = require('path')
 
 function globbies (dir, cb) {
-  if (!fs.existsSync(path.dirname(dir))) {
-    console.log(`${dir} Not found!`)
-    process.exit(1)
-  }
-
   glob(path.resolve(__dirname, dir), (err, files) => {
     if (err) throw err
-    files.map(file => cb(file))
+    return files.map(file => cb(file))
   })
 }
 
