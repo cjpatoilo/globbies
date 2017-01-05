7097bc4ef47e0645193843ae82d8017a27a2a16b
CJ Patoilo <cjpatoilo@gmail.com>
2017-01-04 21:46:54 -0300

    Improve main exporting function

--- index.js
+++ index.js
@@ -2,11 +2,18 @@
 const glob = require('glob')
 const path = require('path')
 
+/**
+ * Main exporting function
+ *
+ * @param {String} promise
+ * @returns {Callback} an instance of a resolver
+ */
 function globbies (dir, cb) {
-  glob(path.resolve(__dirname, dir), (err, files) => {
-    if (err) throw err
-    return files.map(file => cb(file))
-  })
+	dir = path.resolve(__dirname, dir)
+	glob(dir, (err, data) => {
+		if (err) throw err
+		data.map(data => cb(err, data))
+	})
 }
 
 module.exports = globbies
